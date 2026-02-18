7ed430ba990948337c2e6344f0014b3fd61749ad
Soulaymane <soulaymanechourak19@.com>
2026-02-18 11:48:07 +0000

    feat: complete User Module (auth, roles, user management)

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -44,7 +44,6 @@ class User extends Authenticatable
     {
         return [
             'email_verified_at' => 'datetime',
-            'password' => 'hashed',
             'role' => 'string',
         ];
     }
